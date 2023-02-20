51fd676d43e1f1e3f620c9277233d47c071dfcb5
Deepali-Ghadia <deepalinghadia1606@gmail.com>
2023-02-20 17:51:51 +0530

    learned to check datatype using typeof

---
+++ JS/Chapter-6/data_types.js
@@ -0,0 +1,10 @@
+//typeof -> to return the data type in the javascript
+// typeof name_of_variable;
+
+var name="Deepali Ghadia"; // 'string'
+var sum=100; // 'number'
+var flag=true; // 'boolean'
+var list = ["Apple", "Orange", "Banana"]; // 'object'
+var dict = {1: 'Apple', 2: 'Banana', 3: 'Orange'}; // 'object'
+function sum(){}; // 'function'
+var one; // 'undefined'
