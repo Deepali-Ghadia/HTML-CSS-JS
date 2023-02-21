d8a7f7a1b6566ba71def98ba6aeb41d7767d26b8
Deepali-Ghadia <deepalinghadia1606@gmail.com>
2023-02-21 17:58:04 +0530

    learned about date and operations on date

---
+++ JS/Chapter-8/date.js
@@ -0,0 +1,26 @@
+// creating a new date object
+var curr_date = new Date(); // current date and time
+console.log(curr_date);
+curr_date == "Tue Feb 21 2023 17:02:28 GMT+0530 (India Standard Time)" //
+curr_date === "Tue Feb 21 2023 17:02:28 GMT+0530 (India Standard Time)" //false bcoz one is object and one is string
+typeof curr_date; //object
+
+// converting a date to string format
+
+
+// creating a date object from UTC (Coordinated Universal Time)
+
+
+// formatting a javascript date
+
+
+// get the number of milliseconds elapsed since 1 jan 1970 UTC -> Any time since that date is calculated based on the number of seconds elapsed.
+
+
+// get the current date and time
+
+
+// increment a date object
+
+
+// convert to JSON
