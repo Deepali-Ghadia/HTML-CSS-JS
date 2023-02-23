7e57717b380d39947aaa05161f05f8067d418cd9
Deepali-Ghadia <deepalinghadia1606@gmail.com>
2023-02-23 15:12:59 +0530

    implemented various operators

---
+++ JS/Chapter-14/operators.js
@@ -0,0 +1,11 @@
+// remainder or modulus
+console.log(24%3);
+
+// checking whether a number is even or odd using modulus operator
+var num = 50;
+if (num%2 ==0){
+    console.log("Number is Even");
+}
+else{
+    console.log("Number is Odd");
+}
