f2f2b1554cb6335b3ce99672cd1ef1504e7e3dcb
Deepali-Ghadia <deepalinghadia1606@gmail.com>
2023-02-23 15:16:23 +0530

    code updated

--- JS/Chapter-14/operators.js
+++ JS/Chapter-14/operators.js
@@ -9,3 +9,53 @@ if (num%2 ==0){
 else{
     console.log("Number is Odd");
 }
+
+
+// rounding
+var a = Math.round(2.3);
+var b = Math.round(2.6);
+var c = Math.round(-2.3);
+var d = Math.round(-2.6);
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(Math.floor(2.3));
+console.log(Math.ceil(2.4));
+
+
+// trigonometry
+// will give answers in radians
+console.log(Math.sin(90));
+console.log(Math.asin(90)); // reverse of sine
+console.log(Math.cos(90));
+
+// Bitwise Operators
+var a = 101
+var b = 110
+console.log(a | b);
+console.log(a & b);
+console.log(~ b);
+console.log(a ^ b);
+console.log(b << 1);
+console.log(b >> 1);
+
+
+// Exponentiation
+console.log(Math.pow(2,3));
+
+
+// Random integers and floats
+console.log(Math.random()); // gives a random value in floating number
+
+
+// Get minimum and maximum value
+console.log(Math.min(1, 100));
+console.log(Math.min(-1000, 100));
+
+console.log(Math.max(1, 100));
+console.log(Math.max(-1000, 100));
+
+
+// getting roots of a number
+console.log(Math.sqrt(1024));
