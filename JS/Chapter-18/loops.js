f5270a92adfc572a9a7319599bf62b40198d6efd
Deepali-Ghadia <deepalinghadia1606@gmail.com>
2023-02-23 16:53:32 +0530

    implemented for loop  and for/of loop

---
+++ JS/Chapter-18/loops.js
@@ -0,0 +1,12 @@
+// standard for loops
+for (let i=1; i<10; i++){
+    console.log(i);
+}
+
+// for..of loop
+let language = "JavaScript";
+
+let text = "";
+for (let x of language) {
+text += x;
+} //output  "JavaScript"
